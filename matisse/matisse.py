6f4ae4f23cfae6d53c417ad4a7e1d37ba07b6e24
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-07-08 10:51:08 -0700

    Add error handling of Matisse errors

--- matisse/matisse.py
+++ matisse/matisse.py
@@ -10,18 +10,23 @@ class Matisse:
         resource_manager = visa.ResourceManager()
         try:
             self.instrument = resource_manager.open_resource(self.DEVICE_ID)
+            self.query("ERROR:CLEAR")  # start with a clean slate
         except VisaIOError as ioerr:
             raise IOError("Can't reach Matisse. Make sure it's on and connected via USB.") from ioerr
 
-    def query(self, command='', numeric_result=False):
+    def query(self, command='', numeric_result=False, raise_on_error=True):
         """
         Send a command to the Matisse and return the response, optionally converting to a floating-point value.
+        Will optionally raise Python error if a Matisse error occurs.
 
         :param command: the command to send
         :param numeric_result: whether to convert the second portion of the result to a float
+        :param raise_on_error: whether to raise Matisse errors in Python runtime
         :return: the response from the Matisse to the given command
         """
-        result = self.instrument.query(command).strip()
+        result: str = self.instrument.query(command).strip()
+        if result.startswith("!ERROR") and raise_on_error:
+            raise RuntimeError("Error executing command. ", self.query("ERROR:CODE?"))
         if numeric_result:
-            result = float(result.split[1])
+            result: float = float(result.split()[1])
         return result
