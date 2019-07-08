a1be0b21955461013198f8df9cb44d3de2685f88
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-07-08 14:14:20 -0700

    Add setup.py with basic package information

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='matisse_controller',
+      version='0.1',
+      url='http://github.com/lucis-fluxum/matisse_controller',
+      author='Luc Street',
+      license='MIT',
+      packages=find_packages())
