3ca22cf16f235a98f37ca19d9c8895559445fd12
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-07-11 11:59:39 -0700

    Clarify interface with access modifiers, add TODOs

--- matisse/scans_plot.py
+++ matisse/scans_plot.py
@@ -4,25 +4,25 @@ import matplotlib.pyplot as plt
 
 class ScansPlot:
     def __init__(self):
-        self.birefringent_scan_plot: Axes = plt.subplot(2, 1, 1)
-        self.thin_etalon_scan_plot: Axes = plt.subplot(2, 1, 2)
+        self._birefringent_scan_plot: Axes = plt.subplot(2, 1, 1)
+        self._thin_etalon_scan_plot: Axes = plt.subplot(2, 1, 2)
 
     def plot_birefringent_scan(self, positions, voltages):
-        bsp = self.birefringent_scan_plot
-        bsp.set_title('Power Diode Voltage vs. BiFi Motor Position')
-        bsp.set_xlim(positions[0], positions[-1])
-        bsp.set_xlabel('Position')
-        bsp.set_ylabel('Voltage (V)')
-        bsp.plot(positions, voltages)
+        axes = self._birefringent_scan_plot
+        axes.set_title('Power Diode Voltage vs. BiFi Motor Position')
+        axes.set_xlim(positions[0], positions[-1])
+        axes.set_xlabel('Position')
+        axes.set_ylabel('Voltage (V)')
+        axes.plot(positions, voltages)
         print(positions)
         print(voltages)
 
     def plot_thin_etalon_scan(self, positions, voltages):
-        tep = self.thin_etalon_scan_plot
-        tep.set_title('Thin Etalon Reflex Voltage vs. Thin Etalon Motor Position')
-        tep.set_xlim(positions[0], positions[-1])
-        tep.set_xlabel('Position')
-        tep.set_ylabel('Voltage (V)')
-        tep.plot(positions, voltages)
+        axes = self._thin_etalon_scan_plot
+        axes.set_title('Thin Etalon Reflex Voltage vs. Thin Etalon Motor Position')
+        axes.set_xlim(positions[0], positions[-1])
+        axes.set_xlabel('Position')
+        axes.set_ylabel('Voltage (V)')
+        axes.plot(positions, voltages)
         print(positions)
         print(voltages)
