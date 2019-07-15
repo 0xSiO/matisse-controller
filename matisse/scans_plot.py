9d7299f6a2f2f9c80987e3faeab009493be7f010
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-07-15 09:43:54 -0700

    Check motor status before each scan step, add sample scan data

--- matisse/scans_plot.py
+++ matisse/scans_plot.py
@@ -9,6 +9,7 @@ class ScansPlot:
 
     def plot_birefringent_scan(self, positions, voltages):
         axes = self._birefringent_scan_plot
+        axes.cla()
         axes.set_title('Power Diode Voltage vs. BiFi Motor Position')
         axes.set_xlim(positions[0], positions[-1])
         axes.set_xlabel('Position')
@@ -19,6 +20,7 @@ class ScansPlot:
 
     def plot_thin_etalon_scan(self, positions, voltages):
         axes = self._thin_etalon_scan_plot
+        axes.cla()
         axes.set_title('Thin Etalon Reflex Voltage vs. Thin Etalon Motor Position')
         axes.set_xlim(positions[0], positions[-1])
         axes.set_xlabel('Position')
