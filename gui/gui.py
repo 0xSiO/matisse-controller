bbc654aaca9eab5c0d96b7759a24b28ffdef6eab
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-07-16 10:27:17 -0700

    Add notes on more features to add, continue work on Gui

--- gui/gui.py
+++ gui/gui.py
@@ -1,48 +1,63 @@
-from PyQt5.QtWidgets import QApplication, QVBoxLayout, QMainWindow, QWidget, QTextEdit, QMenu, QAction
+from PyQt5.QtWidgets import QApplication, QVBoxLayout, QMainWindow, QWidget, QTextEdit
 
 
-class Gui(QApplication):
+class Gui(QMainWindow):
     def __init__(self):
-        super().__init__([])
-        main_window = QMainWindow()
-        layout = QVBoxLayout()
+        super().__init__()
+        self.setup_menus()
+
         console_area = QTextEdit()
-
-        main_window.setWindowTitle('Matisse Controller')
-
-        set_menu = QMenu('Set')
-        set_wavelength_action = QAction('Wavelength')
-        set_bifi_motor_pos_action = QAction('BiFi Motor Position')
-        set_thin_eta_motor_pos_action = QAction('Thin Etalon Motor Position')
-        set_menu.addActions([set_wavelength_action, set_bifi_motor_pos_action,
-                             set_thin_eta_motor_pos_action])
-
-        scan_menu = QMenu('Scan')
-        bifi_scan_action = QAction('Birefringent Filter')
-        thin_eta_scan_action = QAction('Thin Etalon')
-        scan_menu.addActions([bifi_scan_action, thin_eta_scan_action])
-
-        lock_menu = QMenu('Lock')
-        lock_action = QAction('Set Locked?')
-        lock_action.setCheckable(True)
-        lock_action.toggled.connect(lambda lock: print(lock))
-        lock_menu.addActions([lock_action])
-
-        menu_bar = main_window.menuBar()
-        menu_bar.addMenu(set_menu)
-        menu_bar.addMenu(scan_menu)
-        menu_bar.addMenu(lock_menu)
-
         console_area.setReadOnly(True)
 
+        layout = QVBoxLayout()
         layout.addWidget(console_area)
 
         container = QWidget()
         container.setLayout(layout)
-        main_window.setCentralWidget(container)
-        main_window.resize(600, 200)
-        main_window.show()
-        self.exec()
+        self.setWindowTitle('Matisse Controller')
+        self.setCentralWidget(container)
+        self.resize(600, 200)
+        self.show()
+
+    def setup_menus(self):
+        menu_bar = self.menuBar()
+
+        set_menu = menu_bar.addMenu('Set')
+        self.set_wavelength_action = set_menu.addAction('Wavelength')
+        self.set_bifi_motor_pos_action = set_menu.addAction('BiFi Motor Position')
+        self.set_thin_eta_motor_pos_action = set_menu.addAction('Thin Etalon Motor Position')
+
+        scan_menu = menu_bar.addMenu('Scan')
+        self.bifi_scan_action = scan_menu.addAction('Birefringent Filter')
+        self.thin_eta_scan_action = scan_menu.addAction('Thin Etalon')
+
+        lock_menu = menu_bar.addMenu('Lock')
+        self.lock_all_action = lock_all_action = lock_menu.addAction('Lock All')
+        lock_all_action.setCheckable(True)
+        lock_all_action.toggled.connect(self.lock_all)
+        self.lock_slow_piezo_action = lock_slow_piezo_action = lock_menu.addAction('Lock Slow Piezo')
+        lock_slow_piezo_action.setCheckable(True)
+        self.lock_thin_etalon_action = lock_thin_etalon_action = lock_menu.addAction('Lock Thin Etalon')
+        lock_thin_etalon_action.setCheckable(True)
+        self.lock_piezo_etalon_action = lock_piezo_etalon_action = lock_menu.addAction('Lock Piezo Etalon')
+        lock_piezo_etalon_action.setCheckable(True)
+        self.lock_fast_piezo_action = lock_fast_piezo_action = lock_menu.addAction('Lock Fast Piezo')
+        lock_fast_piezo_action.setCheckable(True)
+
+    def lock_all(self, lock):
+        if lock:
+            for action in [self.lock_slow_piezo_action, self.lock_thin_etalon_action, self.lock_piezo_etalon_action,
+                           self.lock_fast_piezo_action]:
+                action.setChecked(True)
+                action.setEnabled(False)
+        else:
+            for action in [self.lock_slow_piezo_action, self.lock_thin_etalon_action, self.lock_piezo_etalon_action,
+                           self.lock_fast_piezo_action]:
+                action.setChecked(False)
+                action.setEnabled(True)
 
 
-Gui()
+if __name__ == '__main__':
+    app = QApplication([])
+    gui = Gui()
+    app.exec()
