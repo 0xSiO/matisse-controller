78f489e82cf514c9a51f4960828d47cab99bb52d
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-07-17 13:27:45 -0700

    Start adding reboot capability, add graceful exit from logging thread

--- gui/logging.py
+++ gui/logging.py
@@ -3,8 +3,9 @@ from queue import Queue
 from PyQt5.QtCore import pyqtSignal, QThread
 
 
-# Stream to replace sys.stdout
 class LoggingStream:
+    """A basic stream that is meant to replace sys.stdout."""
+
     def __init__(self, queue: Queue):
         self.queue = queue
 
@@ -12,11 +13,13 @@ class LoggingStream:
         self.queue.put(text)
 
 
-# A QThread which waits for data to come through a Queue. It blocks until data is available, then sends it to the UI
-# thread by emitting a Qt signal.
-#
-# Do not implement Qt slots in this class as they will be executed in the creating thread for this class.
 class LoggingThread(QThread):
+    """
+    A QThread which waits for data to come through a Queue. It blocks until data is available, then sends it to the UI
+    thread by emitting a Qt signal.
+
+    Do not implement Qt slots in this class as they will be executed in the creating thread for this class.
+    """
     message_received = pyqtSignal(str)
 
     def __init__(self, queue: Queue, *args, **kwargs):
@@ -25,6 +28,13 @@ class LoggingThread(QThread):
 
     def run(self):
         while True:
-            # TODO: Consider implementing a graceful exit, currently this thread just gets terminated on program exit
             text = self.queue.get()
-            self.message_received.emit(text)
+            if isinstance(text, LoggingExitFlag):
+                break
+            else:
+                self.message_received.emit(text)
+
+
+class LoggingExitFlag:
+    """Push an instance of this class to the logging queue to exit from the LoggingThread event loop."""
+    pass
