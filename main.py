11fae251840810a82221197ca3e25d9917e67368
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-07-17 13:46:14 -0700

    Finish adding restart action to GUI
    - Graceful exit from the logging thread is now in ControlApplication#__del__
    - Small refactor for style purposes

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from contextlib import redirect_stdout
 
 from gui import ControlApplication
-from gui.logging import LoggingExitFlag
 
-exit_code = ControlApplication.EXIT_CODE_REBOOT
-while exit_code == ControlApplication.EXIT_CODE_REBOOT:
+exit_code = ControlApplication.EXIT_CODE_RESTART
+while exit_code == ControlApplication.EXIT_CODE_RESTART:
     gui = ControlApplication([])
     with redirect_stdout(gui.log_stream):
         exit_code = gui.exec()
-        gui.log_queue.put(LoggingExitFlag())
     del gui
