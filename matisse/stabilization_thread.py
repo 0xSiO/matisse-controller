c096e991004df5f98e431a7b5dec57a8c8cfc9d4
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-07-18 10:16:03 -0700

    Add limit-checking code to stabilization thread

--- matisse/stabilization_thread.py
+++ matisse/stabilization_thread.py
@@ -34,21 +34,34 @@ class StabilizationThread(threading.Thread):
                     if drift < 0:
                         # measured wavelength is too high
                         print(f"Too high, decreasing. Drift is {drift}, RefCell pos {self._matisse.query('SCAN:NOW?', numeric_result=True)}")
-                        next_pos = self._matisse.query('SCAN:NOW?', numeric_result=True) - 0.001
-                        # TODO: Don't just check the refcell limit, check all the piezos and correct if needed
-                        if next_pos > 0.05:
-                            # TODO: Use Matisse#set_refcell_pos
-                            # self.matisse.query(f"SCAN:NOW {next_pos}")
-                            pass
+                        if not self.limits_reached():
+                            next_pos = self._matisse.query('SCAN:NOW?', numeric_result=True) - 0.001
+                            self._matisse.set_refcell_pos(next_pos)
+                        else:
+                            print('WARNING: One or more motor limits have been reached. Stopping stabilization for manual correction.')
+                            break
                     else:
                         # measured wavelength is too low
                         print(f"Too low, increasing.   Drift is {drift}, RefCell pos {self._matisse.query('SCAN:NOW?', numeric_result=True)}")
-                        next_pos = self._matisse.query('SCAN:NOW?', numeric_result=True) + 0.001
-                        if next_pos < 0.7:
-                            # self.matisse.query(f"SCAN:NOW {next_pos}")
-                            pass
+                        if not self.limits_reached():
+                            next_pos = self._matisse.query('SCAN:NOW?', numeric_result=True) + 0.001
+                            self._matisse.set_refcell_pos(next_pos)
+                        else:
+                            print('WARNING: One or more motor limits have been reached. Stopping stabilization for manual correction.')
+                            break
                 else:
                     print(f"Within tolerance.      Drift is {drift}, RefCell pos {self._matisse.query('SCAN:NOW?', numeric_result=True)}")
                 time.sleep(self._delay)
             else:
                 break
+
+    def limits_reached(self):
+        current_refcell_pos = self._matisse.query('SCAN:NOW?', numeric_result=True)
+        current_slow_pz_pos = self._matisse.query('SLOWPIEZO:NOW?', numeric_result=True)
+        current_pz_eta_pos = self._matisse.query('PIEZOETALON:BASELINE?', numeric_result=True)
+
+        # During stabilization, we don't want to hit the true limits of any component, so reduce the range slightly
+        offset = 0.05
+        return (self._matisse.REFERENCE_CELL_LOWER_LIMIT + offset < current_refcell_pos < self._matisse.REFERENCE_CELL_UPPER_LIMIT - offset
+               and self._matisse.SLOW_PIEZO_LOWER_LIMIT + offset < current_slow_pz_pos < self._matisse.SLOW_PIEZO_UPPER_LIMIT - offset
+               and self._matisse.PIEZO_ETALON_LOWER_LIMIT + offset < current_pz_eta_pos < self._matisse.PIEZO_ETALON_UPPER_LIMIT - offset)
