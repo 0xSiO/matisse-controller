d6478cd80acde3d5e6447f900f3b8fe57f9c7d89
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-07-19 11:18:20 -0700

    Create LoggingArea widget to handle log-related tasks

---
+++ gui/logging_area.py
@@ -0,0 +1,25 @@
+from queue import Queue
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QTextCursor
+from PyQt5.QtWidgets import QTextEdit
+
+from .threading import LoggingThread, ExitFlag
+
+
+class LoggingArea(QTextEdit):
+    def __init__(self, messages: Queue, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.messages = messages
+        self.update_thread = LoggingThread(messages)
+        self.update_thread.message_received.connect(self.log_message)
+        self.update_thread.start()
+
+    @pyqtSlot(str)
+    def log_message(self, message):
+        self.moveCursor(QTextCursor.End)
+        self.insertPlainText(message)
+
+    def clean_up(self):
+        self.messages.put(ExitFlag())
+        self.update_thread.wait()
