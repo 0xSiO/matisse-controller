f7cabbfb0a9fa6f1cd3278d44996ecb8b6912e60
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-07-19 11:27:12 -0700

    Simplify cleanup code for StatusMonitor, remove MotorStatusPaused
    - Rearrange and clean up ControlApplication setup code

--- gui/status_monitor.py
+++ gui/status_monitor.py
@@ -1,4 +1,3 @@
-from contextlib import AbstractContextManager
 from queue import Queue
 
 from PyQt5.QtWidgets import QLabel
@@ -9,21 +8,11 @@ from .threading import StatusUpdateThread, ExitFlag
 class StatusMonitor(QLabel):
     def __init__(self, matisse, messages: Queue, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.messages = messages
         self.update_thread = StatusUpdateThread(matisse, messages, parent=self)
         self.update_thread.status_read.connect(self.setText)
         self.update_thread.start()
 
-
-class MotorStatusPaused(AbstractContextManager):
-    def __init__(self, monitor: StatusMonitor):
-        self.monitor = monitor
-
-    def __enter__(self):
-        # TODO: Maybe set a flag to only update wavemeter reading
-        self.monitor.update_thread.stop()
-        print('Stopped status monitor.')
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        # TODO: I don't think you can just start it again. Consider a less destructive method for stopping it.
-        self.monitor.update_thread.start()
-        print('Started status monitor.')
+    def clean_up(self):
+        self.messages.put(ExitFlag())
+        self.update_thread.wait()
