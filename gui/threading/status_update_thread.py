86cec9bcf9e1276fe113d06e598c4799263c009c
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-07-23 12:10:33 -0700

    Add stabilization indicator to status widget

--- gui/threading/status_update_thread.py
+++ gui/threading/status_update_thread.py
@@ -3,7 +3,7 @@ from queue import Queue
 
 from PyQt5.QtCore import QThread, pyqtSignal
 
-from gui import utils
+from gui.utils import red_text, orange_text, green_text
 from gui.threading import ExitFlag
 
 
@@ -33,6 +33,7 @@ class StatusUpdateThread(QThread):
                     pz_eta_pos = self.matisse.query('PIEZOETALON:BASELINE?', numeric_result=True)
                     slow_pz_pos = self.matisse.query('SLOWPIEZO:NOW?', numeric_result=True)
                     refcell_pos = self.matisse.query('SCAN:NOW?', numeric_result=True)
+                    is_stabilizing = self.matisse.is_stabilizing()
                     is_locked = self.matisse.laser_locked()
                     wavemeter_value = self.matisse.wavemeter.get_raw_value()
 
@@ -41,7 +42,8 @@ class StatusUpdateThread(QThread):
                     pz_eta_pos_text = f"Pz Eta:{pz_eta_pos}"
                     slow_pz_pos_text = f"Slow Pz:{slow_pz_pos}"
                     refcell_pos_text = f"RefCell:{refcell_pos}"
-                    locked_text = f"{utils.green_text('LOCKED') if is_locked else utils.red_text('NO LOCK')}"
+                    stabilizing_text = f"Stabilize:{green_text('ON') if is_stabilizing else red_text('OFF')}"
+                    locked_text = f"{green_text('LOCKED') if is_locked else red_text('NO LOCK')}"
                     wavemeter_text = f"Wavemeter:{wavemeter_value}"
 
                     limit_offset = 0.05
@@ -54,23 +56,23 @@ class StatusUpdateThread(QThread):
                     pz_eta_near_limit = not self.matisse.PIEZO_ETALON_LOWER_LIMIT + warn_offset < pz_eta_pos < self.matisse.PIEZO_ETALON_UPPER_LIMIT - warn_offset
 
                     if refcell_at_limit:
-                        refcell_pos_text = utils.red_text(refcell_pos_text)
+                        refcell_pos_text = red_text(refcell_pos_text)
                     elif refcell_near_limit:
-                        refcell_pos_text = utils.orange_text(refcell_pos_text)
+                        refcell_pos_text = orange_text(refcell_pos_text)
 
                     if slow_pz_at_limit:
-                        slow_pz_pos_text = utils.red_text(slow_pz_pos_text)
+                        slow_pz_pos_text = red_text(slow_pz_pos_text)
                     elif slow_pz_near_limit:
-                        slow_pz_pos_text = utils.orange_text(slow_pz_pos_text)
+                        slow_pz_pos_text = orange_text(slow_pz_pos_text)
 
                     if pz_eta_at_limit:
-                        pz_eta_pos_text = utils.red_text(pz_eta_pos_text)
+                        pz_eta_pos_text = red_text(pz_eta_pos_text)
                     elif pz_eta_near_limit:
-                        pz_eta_pos_text = utils.orange_text(pz_eta_pos_text)
+                        pz_eta_pos_text = orange_text(pz_eta_pos_text)
 
-                    status = f"{bifi_pos_text} | {thin_eta_pos_text} | {pz_eta_pos_text} | {slow_pz_pos_text} | {refcell_pos_text} | {locked_text} | {wavemeter_text}"
+                    status = f"{bifi_pos_text} | {thin_eta_pos_text} | {pz_eta_pos_text} | {slow_pz_pos_text} | {refcell_pos_text} | {stabilizing_text} | {locked_text} | {wavemeter_text}"
                 except Exception:
-                    status = utils.red_text('Error reading system status. Please restart if this issue persists.')
+                    status = red_text('Error reading system status. Please restart if this issue persists.')
                 self.status_read.emit(status)
                 time.sleep(1)
             else:
