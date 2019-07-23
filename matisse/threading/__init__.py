7dfcb420c3cb449886ae6066c62264aecac440e2
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-07-23 13:37:05 -0700

    Move scan plotting logic to separate threads and a separate package

---
+++ matisse/threading/__init__.py
@@ -0,0 +1,4 @@
+from .birefringent_filter_scan_plot_thread import BirefringentFilterScanPlotThread
+from .thin_etalon_scan_plot_thread import ThinEtalonScanPlotThread
+
+# TODO: See https://matplotlib.org/api/animation_api.html for real-time plotting
---
+++ matisse/threading/thin_etalon_scan_plot_thread.py
@@ -0,0 +1,39 @@
+import threading
+
+import matplotlib.pyplot as plt
+
+
+class ThinEtalonScanPlotThread(threading.Thread):
+    def __init__(self, positions, voltages, smoothed_data, minima, best_pos, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.positions = positions
+        self.voltages = voltages
+        self.smoothed_data = smoothed_data
+        self.minima = minima
+        self.best_pos = best_pos
+
+    def run(self):
+        self.plot_thin_etalon_scan(self.positions, self.voltages, self.smoothed_data)
+        self.plot_thin_etalon_selection(self.best_pos)
+        self.plot_thin_etalon_minima(self.positions[self.minima], self.smoothed_data[self.minima])
+        self.add_thin_etalon_scan_legend()
+        plt.show()
+
+    def plot_thin_etalon_scan(self, positions, voltages, smoothed_voltages):
+        plt.figure()
+        plt.cla()
+        plt.title('Thin Etalon Reflex Voltage vs. Thin Etalon Motor Position')
+        plt.xlim(positions[0], positions[-1])
+        plt.xlabel('Position')
+        plt.ylabel('Voltage (V)')
+        plt.plot(positions, voltages)
+        plt.plot(positions, smoothed_voltages)
+
+    def plot_thin_etalon_minima(self, positions, voltages):
+        plt.plot(positions, voltages, 'r*')
+
+    def plot_thin_etalon_selection(self, position):
+        plt.axvline(position, 0, 1, color='r', linestyle='--')
+
+    def add_thin_etalon_scan_legend(self):
+        plt.legend(['Raw', 'Smoothed', 'Selection'], loc='upper left')
