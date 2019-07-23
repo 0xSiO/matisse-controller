1c3c499ae79dcea5a2b9dcfcd49cd9b9a41dccef
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-07-23 11:58:37 -0700

    Reorganize and document constants

--- matisse/constants.py
+++ matisse/constants.py
@@ -1,20 +1,25 @@
 # TODO: Check the 'small' scan ranges
 class Constants:
-    # How far to each side should we scan the BiFi?
+    # Scanning ################################################################
+
     BIREFRINGENT_SCAN_RANGE = 300
     BIREFRINGENT_SCAN_RANGE_SMALL = 150
-    # How far apart should each point be spaced when measuring the diode power?
     BIREFRINGENT_SCAN_STEP = 3
+
     THIN_ETALON_SCAN_RANGE = 1000
     THIN_ETALON_SCAN_RANGE_SMALL = 500
     THIN_ETALON_SCAN_STEP = 10
+
+    # Approximate change in wavelength per unit motor movement
     THIN_ETALON_NM_PER_STEP = 7.08589e-05
 
     # TODO: Confirm this parameter is ok to use, flank seems to default to 'left'?
     THIN_ETALON_NUDGE = -50
 
-    # How long to wait, in seconds, before giving up on locking the laser.
-    LOCKING_TIMEOUT = 7.0
+    # Internal Matisse constant. Encoded in the result of a motor's STATUS? command
+    MOTOR_STATUS_IDLE = 0x02
+
+    # Limits ##################################################################
 
     BIREFRINGENT_FILTER_LOWER_LIMIT = 0
     BIREFRINGENT_FILTER_UPPER_LIMIT = 188096
@@ -27,8 +32,12 @@ class Constants:
     SLOW_PIEZO_LOWER_LIMIT = 0
     SLOW_PIEZO_UPPER_LIMIT = 0.7
 
+    # Locking/Correction ######################################################
+
+    # Amount of time to wait, in seconds, before giving up on locking the laser.
+    LOCKING_TIMEOUT = 7.0
+
+    # Default correction positions when a limit is reached
     PIEZO_ETALON_CORRECTION_POS = 0
     SLOW_PIEZO_CORRECTION_POS = 0.35
     REFCELL_CORRECTION_POS = 0.35
-
-    MOTOR_STATUS_IDLE = 0x02
