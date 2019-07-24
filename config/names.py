db256f0f309d905467bde2bbab49cf2024bbeff0
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-07-24 15:31:04 -0700

    Use constants to refer to config options, load/save from config dialog

---
+++ config/names.py
@@ -0,0 +1,23 @@
+MATISSE_DEVICE_ID = 'matisse.device_id'
+
+BIFI_SCAN_RANGE = 'matisse.scanning.birefringent_filter.range'
+BIFI_SCAN_RANGE_SMALL = 'matisse.scanning.birefringent_filter.range_small'
+BIFI_SCAN_STEP = 'matisse.scanning.birefringent_filter.step'
+
+THIN_ETA_SCAN_RANGE = 'matisse.scanning.thin_etalon.range'
+THIN_ETA_SCAN_RANGE_SMALL = 'matisse.scanning.thin_etalon.range_small'
+THIN_ETA_SCAN_STEP = 'matisse.scanning.thin_etalon.step'
+THIN_ETA_NUDGE = 'matisse.scanning.thin_etalon.nudge'
+
+LARGE_WAVELENGTH_DRIFT = 'matisse.scanning.wavelength_drift.large'
+MEDIUM_WAVELENGTH_DRIFT = 'matisse.scanning.wavelength_drift.medium'
+SMALL_WAVELENGTH_DRIFT = 'matisse.scanning.wavelength_drift.small'
+
+LOCKING_TIMEOUT = 'matisse.locking.timeout'
+
+PIEZO_ETA_CORRECTION_POS = 'matisse.correction.piezo_etalon_pos'
+SLOW_PIEZO_CORRECTION_POS = 'matisse.correction.slow_piezo_pos'
+REFCELL_CORRECTION_POS = 'matisse.correction.refcell_pos'
+
+WAVEMETER_PORT = 'wavemeter.port'
+WAVEMETER_PRECISION = 'wavemeter.precision'
