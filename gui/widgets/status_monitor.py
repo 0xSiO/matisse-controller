c4702b1a2acf4be0766a97ec436c084b3143f834
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-07-25 13:50:45 -0700

    Add documentation for gui.widgets.status_monitor

--- gui/widgets/status_monitor.py
+++ gui/widgets/status_monitor.py
@@ -6,7 +6,21 @@ from gui.threading import StatusUpdateThread, ExitFlag
 
 
 class StatusMonitor(QLabel):
+    """
+    A QLabel that runs a StatusUpdateThread, and sets its own text to the emitted results of that thread. Each update
+    executes in the creating thread for instances of this class.
+    """
+
     def __init__(self, matisse, messages: Queue, *args, **kwargs):
+        """
+        Initialize an instance of StatusMonitor.
+
+        :param matisse: an instance of Matisse to be provided to the StatusUpdateThread
+        :type matisse: matisse.Matisse
+        :param messages: a message queue to be given to the StatusUpdateThread
+        :param args: args to pass to QLabel.__init__
+        :param kwargs: kwargs to pass to QLabel.__init__
+        """
         super().__init__(*args, **kwargs)
         self.messages = messages
         self.update_thread = StatusUpdateThread(matisse, messages, parent=self)
