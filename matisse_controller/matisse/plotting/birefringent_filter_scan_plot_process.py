f8a8848dfcb0857a160356d5e9a1e5d8c9028260
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-08-01 15:52:51 -0700

    Make plotting new scan position optional if we decide to stay

--- matisse_controller/matisse/plotting/birefringent_filter_scan_plot_process.py
+++ matisse_controller/matisse/plotting/birefringent_filter_scan_plot_process.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 class BirefringentFilterScanPlotProcess(multiprocessing.Process):
-    def __init__(self, positions, voltages, smoothed_data, maxima, old_pos, best_pos, *args, **kwargs):
+    def __init__(self, positions, voltages, smoothed_data, maxima, old_pos, best_pos, using_new_pos, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.positions = positions
         self.voltages = voltages
@@ -12,6 +12,7 @@ class BirefringentFilterScanPlotProcess(multiprocessing.Process):
         self.maxima = maxima
         self.old_pos = old_pos
         self.best_pos = best_pos
+        self.using_new_pos = using_new_pos
 
     def run(self):
         self.plot_birefringent_scan(self.positions, self.voltages, self.smoothed_data)
@@ -35,7 +36,11 @@ class BirefringentFilterScanPlotProcess(multiprocessing.Process):
 
     def plot_birefringent_selection(self, old_pos, new_pos):
         plt.axvline(old_pos, 0, 1, color='r', linestyle='--')
-        plt.axvline(new_pos, 0, 1, color='r', linestyle='-')
+        if self.using_new_pos:
+            plt.axvline(new_pos, 0, 1, color='r', linestyle='-')
 
     def add_bifi_scan_legend(self):
-        plt.legend(['Raw', 'Smoothed', 'Old Pos', 'New Pos'], loc='upper left')
+        names = ['Raw', 'Smoothed', 'Old Pos']
+        if self.using_new_pos:
+            names.append('New Pos')
+        plt.legend(names, loc='upper left')
--- matisse_controller/matisse/plotting/thin_etalon_scan_plot_process.py
+++ matisse_controller/matisse/plotting/thin_etalon_scan_plot_process.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 class ThinEtalonScanPlotProcess(multiprocessing.Process):
-    def __init__(self, positions, voltages, smoothed_data, minima, old_pos, best_pos, *args, **kwargs):
+    def __init__(self, positions, voltages, smoothed_data, minima, old_pos, best_pos, using_new_pos, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.positions = positions
         self.voltages = voltages
@@ -12,6 +12,7 @@ class ThinEtalonScanPlotProcess(multiprocessing.Process):
         self.minima = minima
         self.old_pos = old_pos
         self.best_pos = best_pos
+        self.using_new_pos = using_new_pos
 
     def run(self):
         self.plot_thin_etalon_scan(self.positions, self.voltages, self.smoothed_data)
@@ -35,7 +36,11 @@ class ThinEtalonScanPlotProcess(multiprocessing.Process):
 
     def plot_thin_etalon_selection(self, old_pos, new_pos):
         plt.axvline(old_pos, 0, 1, color='r', linestyle='--')
-        plt.axvline(new_pos, 0, 1, color='r', linestyle='-')
+        if self.using_new_pos:
+            plt.axvline(new_pos, 0, 1, color='r', linestyle='-')
 
     def add_thin_etalon_scan_legend(self):
-        plt.legend(['Raw', 'Smoothed', 'Old Pos', 'New Pos'], loc='upper left')
+        names = ['Raw', 'Smoothed', 'Old Pos']
+        if self.using_new_pos:
+            names.append('New Pos')
+        plt.legend(names, loc='upper left')
