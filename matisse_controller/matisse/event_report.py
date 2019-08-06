9ef0a891e962cd0cd15c0f81e4114307343cdcc0
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-08-06 09:00:06 -0700

    Make an enum to hold event type information

--- matisse_controller/matisse/event_report.py
+++ matisse_controller/matisse/event_report.py
@@ -3,20 +3,30 @@
 import csv
 import os.path
 from datetime import datetime
+from enum import Enum
 
 FILE_NAME = 'matisse_event_report.csv'
 FIELDS = ['timestamp', 'event_type', 'current_wavelength', 'bifi_pos', 'thin_etalon_pos', 'refcell_pos',
           'piezo_etalon_pos', 'slow_piezo_pos', 'is_stabilizing', 'is_locked', 'other_comments']
 
 
-def log_event(event_type: str, matisse, current_wavelength: float, other_comments=''):
+class EventType(Enum):
+    """An enumeration of different event types. The assigned value goes in each row of the CSV report."""
+    SCAN_LIMIT_EXCEEDED = 'scan_limit_exceeded'
+    LOCK_CORRECTION = 'lock_correction'
+    STABILIZATION_CORRECTION = 'stabilization_correction'
+    STABILIZATION_LIMIT_EXCEEDED = 'stabilization_correction_limit_exceeded'
+    WAVELENGTH_DRIFT = 'wavelength_drift'
+
+
+def log_event(event_type: EventType, matisse, current_wavelength: float, other_comments=''):
     """
     Write an event to the Matisse event report CSV file.
 
     Parameters
     ----------
-    event_type : str
-        a string describing the type of event that occurred
+    event_type : EventType
+        a member of `EventType` describing the type of event that occurred
     matisse : matisse_controller.matisse.matisse.Matisse
     current_wavelength : float
         the wavelength at which this event occurred
@@ -32,7 +42,7 @@ def log_event(event_type: str, matisse, current_wavelength: float, other_comment
         refcell_pos, pz_eta_pos, slow_pz_pos = matisse.get_stabilizing_piezo_positions()
         event_details = {
             'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            'event_type': event_type,
+            'event_type': event_type.value,
             'current_wavelength': current_wavelength,
             'bifi_pos': bifi_pos,
             'thin_etalon_pos': thin_eta_pos,
