e812a7c7e40aa1de94a4aa1e52b63fa8a47d41ee
Luc Street <6709262+0xSiO@users.noreply.github.com>
2019-08-06 12:41:24 -0700

    Refactor ShamrockPLE class

--- matisse_controller/shamrock_ple/shamrock_ple.py
+++ matisse_controller/shamrock_ple/shamrock_ple.py
@@ -1,7 +1,6 @@
+import os
 import pickle
 import time
-from ctypes import *
-from os import path
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,9 +16,6 @@ shamrock = Shamrock()
 class ShamrockPLE:
     """PLE scanning functionality with the Andor Shamrock and Newton CCD."""
 
-    SHAMROCK_ID = c_int(0)
-    """Device ID of Shamrock, assuming it is the only spectrometer connected."""
-
     # TODO: Currently untested
 
     def __init__(self, matisse):
@@ -29,7 +25,7 @@ class ShamrockPLE:
     def start_ple_scan(self, name, initial_wavelength, final_wavelength, step, exposure_time,
                        acquisition_mode=ACQ_MODE_ACCUMULATE, readout_mode=READ_MODE_FVB, temperature=-70):
         """Perform a PLE scan using the Andor Shamrock spectrometer."""
-        if path.exists(f"{name}_full_pickled.dat"):
+        if os.path.exists(f"{name}_full_pickled.dat"):
             raise FileExistsError(
                 f"A PLE scan has already been run for '{name}'. Please choose a different name and try again.")
 
@@ -37,16 +33,11 @@ class ShamrockPLE:
         wavelengths = np.append(np.arange(initial_wavelength, final_wavelength, step), final_wavelength)
         counter = 1
         scans = {}
-        tolerance = 0.001
         for wavelength in wavelengths:
             if self.exit_flag:
                 print('Received exit signal, saving scan data.')
                 break
-            self.matisse.set_wavelength(wavelength)
-            self.matisse.set_recommended_fast_piezo_setpoint()
-            self.matisse.start_laser_lock_correction()
-            while abs(wavelength - self.matisse.wavemeter_wavelength()) >= tolerance:
-                time.sleep(3)
+            self.lock_at_wavelength(wavelength)
             data = ccd.take_acquisition(CCD.WIDTH)  # FVB mode bins counts into each column, so grab points along width
             self.matisse.stop_laser_lock_correction()
             file_name = f"{str(counter).zfill(3)}_{name}_{wavelength}nm_StepSize_{step}nm_Range_{abs(round(final_wavelength - initial_wavelength, 8))}nm.txt"
@@ -57,8 +48,17 @@ class ShamrockPLE:
             pickle.dump(scans, full_data_file)
         ccd.shutdown()
 
+    def lock_at_wavelength(self, wavelength: float):
+        tolerance = 0.001
+        self.matisse.set_wavelength(wavelength)
+        self.matisse.set_recommended_fast_piezo_setpoint()
+        self.matisse.start_laser_lock_correction()
+        while abs(wavelength - self.matisse.wavemeter_wavelength()) >= tolerance:
+            time.sleep(3)
+
     def stop_ple_scan(self):
         print('Stopping PLE scan.')
+        self.matisse.exit_flag = True
         self.exit_flag = True
 
     def analyze_ple_data(self, name):
