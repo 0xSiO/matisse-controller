792e819bf61e432fc4be47251f5191c5340fc59b
Luc Street <lucis-fluxum@users.noreply.github.com>
2019-08-19 13:50:35 -0700

    Use pipes to plot multiple PL spectra during PLE scan

--- matisse_controller/shamrock_ple/plotting/spectrum_plot_process.py
+++ matisse_controller/shamrock_ple/plotting/spectrum_plot_process.py
@@ -1,25 +1,39 @@
 from multiprocessing import Process
+from multiprocessing.connection import Connection
 
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 
 
 class SpectrumPlotProcess(Process):
-    def __init__(self, wavelengths, counts, *args, **kwargs):
+    def __init__(self, wavelengths=None, counts=None, pipe: Connection = None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.wavelengths = wavelengths
         self.counts = counts
+        self.pipe = pipe
+        self.figure: Figure = plt.figure('CCD Acquisition')
+        self.axes: Axes = plt.gca()
+        self.axes.set_title('Counts vs. Wavelength')
+        self.axes.set_xlabel('Wavelength (nm)')
+        self.axes.set_ylabel('Counts')
 
     def run(self):
-        plt.figure('CCD Acquisition')
-        plt.cla()
-        plt.title('Counts vs. Wavelength')
-        plt.xlim(self.wavelengths[0], self.wavelengths[-1])
-        plt.xlabel('Wavelength (nm)')
-        plt.ylabel('Counts')
-        plt.plot(self.wavelengths, self.counts)
+        if self.pipe:
+            try:
+                while self.pipe.poll(timeout=None):
+                    data = self.pipe.recv()
+                    if not data:
+                        break
+                    else:
+                        self.plot_data(*data)
+            except BrokenPipeError:
+                pass
+        else:
+            self.plot_data(self.wavelengths, self.counts)
         plt.show()
 
-    # TODO: This can't run because plt.show() blocks. Use some kind of communication channel instead
-    def add_data(self, wavelengths, counts):
-        plt.plot(wavelengths, counts)
-        plt.draw()
+    def plot_data(self, wavelengths, counts):
+        self.axes.set_xlim(wavelengths[0], wavelengths[-1])
+        self.axes.plot(wavelengths, counts)
+        plt.pause(0.001)
